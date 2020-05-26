761828a737a14d648c61671f0ebb0bb3a1bdced9
G Hutchison <grant.hutchison@tdsb.on.ca>
2020-05-26 14:45:15 -0400

    initial drop

---
+++ TestMe/src/ca/csteacher/Game1.java
@@ -0,0 +1,18 @@
+package ca.csteacher;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game1 extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
